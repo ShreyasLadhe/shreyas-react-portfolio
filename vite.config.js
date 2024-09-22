16085f222c24888d97cf6148ae81ef3de5c4e3cf
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2024-09-22 20:14:40 +0530

    Rollback to v1.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages'; // Import the plugin
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()], // Include the plugin
-  base: 'shreyas-react-portfolio',  // Replace 'your-repo-name' with the name of your GitHub repository
-});
+  plugins: [react()],
+})
