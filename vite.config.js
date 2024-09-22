b1833acb054a8de4ac6c71a2d5a22fa390e5d405
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2024-09-22 20:12:03 +0530

    Integration of GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages'; // Import the plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()], // Include the plugin
+  base: 'shreyas-react-portfolio',  // Replace 'your-repo-name' with the name of your GitHub repository
+});
